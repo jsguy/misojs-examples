90ccf4241bf760fb6c6c8de830fbcfbfef18666c
jsguy <mikkel@pointful.com>
2015-06-26 09:04:47 +1000

    no need for query for js sequence diagrams

--- blog/modules/components/sequencediagram/sequencediagram.component.js
+++ blog/modules/components/sequencediagram/sequencediagram.component.js
@@ -6,20 +6,25 @@ var SequencediagramsComponent = {
 		return args;
 	},
 	view: function(ctrl) {
-		return m("div", {
-			config: SequencediagramsComponent.config(ctrl), 
-			"class": "diagram"},
-			ctrl.value()
+		return m("div",{
+				config: SequencediagramsComponent.config(ctrl), 
+				"class": "diagram"
+			},
+			//	We need this to preserve NL and not use &gt;
+			m("pre", m.trust(ctrl.value()))
 		);
 	},
 	config: function(ctrl) {
 		return function(element, isInitialized) {
-			if(typeof jQuery !== 'undefined' && typeof jQuery.fn.sequenceDiagram !== 'undefined') {
+			if(typeof Diagram !== 'undefined') {
 				if (!isInitialized) {
-					$(element).sequenceDiagram({theme: 'hand'});
+					var pre = element.firstChild;
+					var diagram = Diagram.parse(pre.innerText);
+  					diagram.drawSVG(element, {theme: 'hand'});
+  					element.removeChild(pre);
 				}
 			} else {
-				console.warn('ERROR: You need jQuery and js sequence diagrams in the page');	
+				console.warn('ERROR: You need js sequence diagrams in the page');
 			}
 		};
 	}
